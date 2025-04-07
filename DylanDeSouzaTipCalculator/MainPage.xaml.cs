375a320fb873db545a78c818a05a590a44159fa4
Learner062022 <ddylan246@yahoo.com>
2025-04-07 14:12:47 +0800

    feat(tip-calculator): add tip slider and split bill functionality
    - Added Syncfusion slider for tip percentage input.
    - Implemented binding for bill amount, tip amount, total, and split amount.
    - Introduced diners stepper for splitting the bill between multiple people.
    - Updated logic for handling decimal inputs and numeric values.
    - Enhanced reset functionality for clearing inputs.

---
+++ DylanDeSouzaTipCalculator/MainPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DylanDeSouzaTipCalculator
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        bool decimalButtonPressed;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) => 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        Model model = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public string BillAmount
+        {
+            get => model.BillAmount;
+            set
+            {
+                if (model.BillAmount != value)
+                {
+                    model.BillAmount = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(TipAmount));
+                    OnPropertyChanged(nameof(Total));
+                    OnPropertyChanged(nameof(SplitAmount));
+                }
+            }
+        }
+
+        public double Total => model.Total;
+
+        public double TipAmount => model.TipAmount; 
+
+        public double SplitAmount => model.SplitAmount;
+
+        public int AmountDiners
+        {
+            get => model.AmountDiners; 
+            set
+            {
+                if (model.AmountDiners != value)
+                {
+                    model.AmountDiners = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(SplitAmount));
+                }
+            }
+        }
+
+        public double TipPercentage
+        {
+            get => model.TipPercentage;
+            set
+            {
+                if (model.TipPercentage != value)
+                {
+                    model.TipPercentage = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(TipAmount));
+                    OnPropertyChanged(nameof(Total));
+                    OnPropertyChanged(nameof(SplitAmount));
+                }
+            }
+        }
+
+        void Reset()
+        {
+            BillAmount = "0";  
+            AmountDiners = 0;
+            model.AmountDecimalNumbers = 0; 
+            decimalButtonPressed = false;
+            OnPropertyChanged(nameof(TipAmount));
+            OnPropertyChanged(nameof(Total));
+            OnPropertyChanged(nameof(AmountDiners));
+        }
+
+        void HandleDecimalInput()
+        {
+            if (decimalButtonPressed) return;
+            decimalButtonPressed = true;
+            BillAmount += ".";
+            return;
+        }
+
+        void HandleNumericInput(string value)
+        {
+            if (decimalButtonPressed)
+            {
+                model.AmountDecimalNumbers += 1;
+                if (model.AmountDecimalNumbers > 2) return;
+            }
+            if (BillAmount == "0") BillAmount = value;
+            else BillAmount += value;
+
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Text)
+            {
+                case "C":
+                    Reset();
+                    break;
+
+                case ".":
+                    HandleDecimalInput();
+                    break;
+
+                case "0":
+                    if (BillAmount == "0") break;
+                    HandleNumericInput(button.Text); 
+                    break;
+
+                default:
+                    HandleNumericInput(button.Text); 
+                    break;
+            }
+        }
+    }
+}
---
+++ DylanDeSouzaTipCalculator/Model.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DylanDeSouzaTipCalculator
+{
+    public class Model
+    {
+        string billAmount = "0";
+        int amountDiners = 0;
+        int amountDecimalNumbers = 0;
+        double tipPercentage;
+
+        public string BillAmount
+        {
+            get => billAmount;
+            set => billAmount = value;
+        }
+
+        public int AmountDiners
+        {
+            get => amountDiners;
+            set => amountDiners = value;
+        }
+
+        public int AmountDecimalNumbers
+        {
+            get => amountDecimalNumbers;
+            set => amountDecimalNumbers = value;
+        }
+
+        public double TipPercentage
+        {
+            get => tipPercentage;
+            set => tipPercentage = value;
+        }
+
+        public double TipAmount => TipPercentage * double.Parse(BillAmount);
+
+        public double Total => TipAmount + double.Parse(BillAmount);
+
+        public double SplitAmount => AmountDiners > 0 ? Total / AmountDiners : 0;
+    }
+}
