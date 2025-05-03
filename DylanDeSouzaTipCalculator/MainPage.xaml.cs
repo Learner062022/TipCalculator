3fac819c995943db4f363035ec26dadc9c74c87e
Learner062022 <ddylan246@yahoo.com>
2025-05-03 14:57:50 +0800

    refactor(ui): simplify MainPage logic by delegating state management to model
    - Remove INotifyPropertyChanged from MainPage.
    - Bind UI directly to the Model for improved separation of concerns.
    - Delegate state updates to the Model methods for cleaner architecture.
    - Improve code maintainability by reducing redundancy in property updates.
    - Streamline event handling within Button_Clicked.
    
    This refactor enhances modularity, ensuring a more maintainable and scalable UI structure.

--- DylanDeSouzaTipCalculator/MainPage.xaml.cs
+++ DylanDeSouzaTipCalculator/MainPage.xaml.cs
@@ -3,104 +3,14 @@ using System.Runtime.CompilerServices;
 
 namespace DylanDeSouzaTipCalculator
 {
-    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    public partial class MainPage : ContentPage
     {
-        bool decimalButtonPressed;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) => 
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
         Model model = new();
 
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
-        }
-
-        public string BillAmount
-        {
-            get => model.BillAmount;
-            set
-            {
-                if (model.BillAmount != value)
-                {
-                    model.BillAmount = value;
-                    OnPropertyChanged();
-                    OnPropertyChanged(nameof(TipAmount));
-                    OnPropertyChanged(nameof(Total));
-                    OnPropertyChanged(nameof(SplitAmount));
-                }
-            }
-        }
-
-        public double Total => model.Total;
-
-        public double TipAmount => model.TipAmount; 
-
-        public double SplitAmount => model.SplitAmount;
-
-        public int AmountDiners
-        {
-            get => model.AmountDiners; 
-            set
-            {
-                if (model.AmountDiners != value)
-                {
-                    model.AmountDiners = value;
-                    OnPropertyChanged();
-                    OnPropertyChanged(nameof(SplitAmount));
-                }
-            }
-        }
-
-        public double TipPercentage
-        {
-            get => model.TipPercentage;
-            set
-            {
-                if (model.TipPercentage != value)
-                {
-                    model.TipPercentage = value;
-                    OnPropertyChanged();
-                    OnPropertyChanged(nameof(TipAmount));
-                    OnPropertyChanged(nameof(Total));
-                    OnPropertyChanged(nameof(SplitAmount));
-                }
-            }
-        }
-
-        void Reset()
-        {
-            BillAmount = "0";  
-            AmountDiners = 0;
-            model.AmountDecimalNumbers = 0; 
-            decimalButtonPressed = false;
-            OnPropertyChanged(nameof(TipAmount));
-            OnPropertyChanged(nameof(Total));
-            OnPropertyChanged(nameof(AmountDiners));
-        }
-
-        void HandleDecimalInput()
-        {
-            if (decimalButtonPressed) return;
-            decimalButtonPressed = true;
-            BillAmount += ".";
-            return;
-        }
-
-        void HandleNumericInput(string value)
-        {
-            if (decimalButtonPressed)
-            {
-                model.AmountDecimalNumbers += 1;
-                if (model.AmountDecimalNumbers > 2) return;
-            }
-            if (BillAmount == "0") BillAmount = value;
-            else BillAmount += value;
-
+            BindingContext = model;
         }
 
         void Button_Clicked(object sender, EventArgs e)
@@ -110,20 +20,19 @@ namespace DylanDeSouzaTipCalculator
             switch (button.Text)
             {
                 case "C":
-                    Reset();
+                    model.Reset();
                     break;
 
                 case ".":
-                    HandleDecimalInput();
+                    model.HandleDecimalInput();
                     break;
 
                 case "0":
-                    if (BillAmount == "0") break;
-                    HandleNumericInput(button.Text); 
+                    model.HandleNumericInput(button.Text);
                     break;
 
                 default:
-                    HandleNumericInput(button.Text); 
+                    model.HandleNumericInput(button.Text);
                     break;
             }
         }
